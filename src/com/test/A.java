f93a0d1562b972a2a564def2a22324da776afcfa
lule <1721753977@qq.com>
2019-12-30 13:59:53 +0800

    third commit content hellochina A.class

--- src/com/test/A.java
+++ src/com/test/A.java
@@ -4,5 +4,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("hellogit");
         System.out.println("helloworld");
+        System.out.println("hellochina");
     }
 }
